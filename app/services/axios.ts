572d6decf7f04cfbcc9833367363a957078939e5
Kauan Vieira Xavier <kauanvieiraxavierk@gmail.com>
2024-02-18 17:42:49 -0300

    feat:: before making dangerous changes

--- app/services/axios.ts
+++ app/services/axios.ts
@@ -1,14 +1,1 @@
-import axios from 'axios';
-import { parseCookies } from 'nookies';
-export const getAPIClient = (ctx?: any) => {
-  const { 'admin.auth': token } = parseCookies(ctx);
-
-  const api = axios.create({
-    baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
-  });
-
-  if (token) {
-    api.defaults.headers['Authorization'] = `Bearer ${token}`;
-  }
-  return api;
-};
+export const getAPIClient = (ctx?: any) => {};
